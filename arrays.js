4a158ee9252c6d1719377ed30d644a9005fc116a
Devin Moore <devin.moore@volken.org>
2017-11-29 02:46:36 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,7 +2,7 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(array, element) {
   const originalArray = []
-  originalArray.unshift(element)=newArray
+  originalArray.unshift(element)= var newArray
   return newArray
 }
 
