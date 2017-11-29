2d946730610246d5865807c67e96f497c73ce300
Devin Moore <devin.moore@volken.org>
2017-11-29 03:09:27 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -20,4 +20,6 @@ function destructivelyAddElementToEndOfArray(array, element) {
   return array;
 }
 
-function accessElementInArray
+function accessElementInArray(array, index) {
+  return array[index];
+}
