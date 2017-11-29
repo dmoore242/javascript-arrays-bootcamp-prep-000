2cd2888ebf021926977873de1246cf849860ea2d
Devin Moore <devin.moore@volken.org>
2017-11-29 03:25:48 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -30,6 +30,6 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 }
 
 function removeElementFromBeginningOfArray(array) {
-  array = array.slice(0);
+  array = array.slice(0,0);
   return array
 }
